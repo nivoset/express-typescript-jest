0f0874dd830fdee8c409f79b2f4f8568cf92bf49
Ben Koop <nivoset@gmail.com>
2020-02-15 14:23:35 -0500

    db logging

--- src/mock-database/index.ts
+++ src/mock-database/index.ts
@@ -1,18 +1,23 @@
 import User from "../types/User"
 import uuid from "uuid";
+import {getLogger} from '../log';
+
+const log = getLogger(__filename);
 
 let userList: User[] = [];
 
-// const removeUser = (userId) => ({id}) => id !== userId; ???
-const removeUser = (userId: string): ((value: User) => boolean) => 
-		({id} : User): boolean => id !== userId;
+// const removeUser = (userId) => ({id}) => id !== userId; did i get this right?
+const removeUser = (userId : string): 
+        ((value: User) => boolean) => ({id} : User): boolean => id !== userId;
 
 export default {
-    getAll: () : User[] => [...userList],
+    getAll: () : User[] => [...userList.map((user : User) => ({...user}))],
     addUser: (user : User) : number => userList.push({...user, id: uuid()}),
-    removeUserById: (userId: string): boolean => {
+    removeUserById: (userId: string) : boolean => {
+        log.debug(`Removing userId=${userId}`);
         const originalLength = userList.length;
         userList = userList.filter(removeUser(userId));
+
         return (originalLength > userList.length);
     }
 }
