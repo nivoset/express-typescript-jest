4a84dad336f20f978715153efbd94b8df4304064
Ben Koop <nivoset@gmail.com>
2020-02-20 06:36:36 -0500

    winston express logger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express" ;
 import users from "./route/users";
 import path from "path";
+import { routeLogging } from "./log"
 const app = express();
 
+app.use(routeLogging);
 //users route
 app.use("/users", users);
 //static html being served
